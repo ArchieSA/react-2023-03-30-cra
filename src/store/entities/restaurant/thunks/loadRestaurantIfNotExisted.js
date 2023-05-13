790e985374b2d1024719f96055fb2ab318adfe30
s-archie-dev <s.artman.dev@gmail.com>
2023-05-13 16:27:08 +0300

    Project with cra

---
+++ src/store/entities/restaurant/thunks/loadRestaurantIfNotExisted.js
@@ -0,0 +1,16 @@
+import { selectRestaurantIds } from "../selectors";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import {LOADING_STATUS} from "../../../../constants/loading-status";
+
+export const fetchRestaurants = createAsyncThunk(
+  "restaurant/fetchRestaurants",
+  async (_, { getState, rejectWithValue }) => {
+    if (selectRestaurantIds(getState()).length) {
+      return rejectWithValue(LOADING_STATUS.earlyAdded);
+    }
+
+    const response = await fetch("http://localhost:3001/api/restaurants/");
+
+    return await response.json();
+  }
+);
